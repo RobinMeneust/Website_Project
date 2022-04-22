bd3014228e85768310040b061dc6fda3d10f14a7
RobinMeneust <85349121+RobinMeneust@users.noreply.github.com>
2022-04-22 20:24:50 +0200

    v0 [committed by multiple users]
    First version of the website
    Only the home page was created (WIP)

---
+++ LaFleurWebsite/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <link rel="stylesheet" type="text/css" href="./css/index.css">
+	</head>
+	<body>
+        <div class="wrapper">
+            <header>
+                <table class="tableHeader">
+                    <tr>
+                        <th class="tier logo"><img src="./img/logo.png" alt="NOM DU SITE" width="120vw"></th>
+                        <th class="tier"><h1 class="siteName">Nom Du Site</h1></th>
+                        <th class="tier"><div class="login_Cart">
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff"><path d="M10 12c-7.3 0-9 5.5-9 5.5v1h18v-1S17.3 12 10 12z"/><circle cx="10" cy="6" r="5"/></svg> Connexion <br>
+</div></th>
+                    </tr>
+                    <tfoot><tr>
+                        <th colspan="3"><ul style="font-size:30px; color:white;">
+                            <il>accueil</il>
+                            <il>Bubles</il>
+                            <il>Rosiers</il>
+                            <il>Plantes à massif</il>
+                            <il>Contact</il>
+                        </ul></th>
+                    </tr></tfoot>   
+                </table>
+            </header>
+            <div class="base">
+                <div class="box menu"><h1>ceci est une phrase, voilà, bravo tu sais lire.</h1></div>
+                <div class="box mainPart"><h1>La fleur</h1></div>
+            </div>
+            <footer>
+                <h4>footer</h4>
+            </footer>
+        </div>
+	</body>
+</html>
