ff9aa2eba860bd65c0a5971a4734f35d55263dcd
RobinMeneust <85349121+RobinMeneust@users.noreply.github.com>
2022-04-23 14:17:57 +0200

    Changed the theme
    - Placeholders have been added to replace the "random" texts, titles and images
    - Vertical and horizontal rules have been added
    - Changed the theme to dark mode
    - Fixed some issues and changed the name of some classes and elements

--- LaFleurWebsite/index.php
+++ LaFleurWebsite/index.php
@@ -1,37 +1,61 @@
 <!DOCTYPE html>
 <html>
 	<head>
-        <link rel="stylesheet" type="text/css" href="./css/index.css">
+		<title>Accueil</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
 	</head>
 	<body>
-        <div class="wrapper">
-            <header>
-                <table class="tableHeader">
-                    <tr>
-                        <th class="tier logo"><img src="./img/logo.png" alt="NOM DU SITE" width="120vw"></th>
-                        <th class="tier"><h1 class="siteName">Nom Du Site</h1></th>
-                        <th class="tier"><div class="login_Cart">
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff"><path d="M10 12c-7.3 0-9 5.5-9 5.5v1h18v-1S17.3 12 10 12z"/><circle cx="10" cy="6" r="5"/></svg> Connexion <br>
-</div></th>
-                    </tr>
-                    <tfoot><tr>
-                        <th colspan="3"><ul style="font-size:30px; color:white;">
-                            <il>accueil</il>
-                            <il>Bubles</il>
-                            <il>Rosiers</il>
-                            <il>Plantes à massif</il>
-                            <il>Contact</il>
-                        </ul></th>
-                    </tr></tfoot>   
-                </table>
-            </header>
-            <div class="base">
-                <div class="box menu"><h1>ceci est une phrase, voilà, bravo tu sais lire.</h1></div>
-                <div class="box mainPart"><h1>La fleur</h1></div>
-            </div>
-            <footer>
-                <h4>footer</h4>
-            </footer>
-        </div>
+		<div class="wrapper">
+			<header>
+				<table class="tableHeader">
+					<tr>
+						<td class="logo"><img src="./img/logo.png" alt="nom du site" width="80%"></td>
+						<td><h1 class="title">LA FLEUR</h1></td>
+						<td>
+							<div class="loginAndCart">
+								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff"><path d="M10 12c-7.3 0-9 5.5-9 5.5v1h18v-1S17.3 12 10 12z"/><circle cx="10" cy="6" r="5"/></svg> Connexion <br>
+							</div>
+						</td>
+					</tr>
+					<tfoot>
+						<tr>
+						<td colspan="3">
+							<nav>
+								<ul>
+									<li class="navElement" id="firstNavElement"><a class="navElement" href="index.php">Accueil</a></li>
+									<li class="navElement"><a class="navElement" href="WIP.php">Bulbes</a></li>
+									<li class="navElement"><a class="navElement" href="WIP.php">Rosiers</a></li>
+									<li class="navElement" id="largeNavElement"><a class="navElement" href="WIP.php">Plantes à massif</a></li>
+									<li class="navElement"><a class="navElement" href="contact.html">Contact</a></li>
+								</ul>
+							</nav>
+						</td>
+					</tr></tfoot>
+				</table>
+			</header>
+			<div class="base">
+				<div class="box menu">
+					<p>Placeholder menu</p>
+				</div>
+				<div class="verticalRule"></div>
+				<div class="box mainPart">
+					<h1>PLACEHOLDER TITLE</h1><br>
+					<p>
+						Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder <br>
+						Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder <br>
+						Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder <br>
+					</p>
+				</div>
+			</div>
+			<footer>
+				<div class="footerContent">
+					<h4>footer title placeholder</h4><br>
+					<address>
+						placeholder<br>
+					</address>
+				</div>
+			</footer>
+		</div>
 	</body>
 </html>
