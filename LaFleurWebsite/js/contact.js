ce7b808e73623804eff1add0dba519fc4fdb162f
RobinMeneust <robin.meneust@hotmail.fr>
2022-05-11 22:32:14 +0200

    Improved the contact form

--- LaFleurWebsite/js/contact.js
+++ LaFleurWebsite/js/contact.js
@@ -1,11 +1,28 @@
-function checkIfEmpty(element){
-    format_info_box = document.getElementById(element.id+"_format");
-    if(element.value.trim()==""){
-        element.style.border="solid red 1px";
-        format_info_box.style.display="inline";
+function checkStringFormat(element){
+    let isNotValid = false;
+    let parent = element.parentNode;
+    let isActive = document.getElementById(element.id+"_format");
+    //const regexWithAccents = new RegExp('[A-zÀ-ú0-9\-_@]*');
+    ///^([a-z0-9]{5,})$/
+    //const regexWithoutAccents = new RegExp('[A-z0-9\-_@]*');
+
+    isNotValid = ! (/^([A-zÀ-ú0-9\-@]*)$/.test(element.value));
+    isNotValid |= / /.test(element.value);
+    if(isNotValid){
+        if(!isActive){ // if format_info_box doesn't exists
+            element.style.border = "solid red 1px";
+            let format_info_box = document.createElement("span");
+            format_info_box.innerHTML = "Ne doit pas contenir de caractères spéciaux"
+            format_info_box.style.color = "red";
+            format_info_box.id=element.id+"_format"
+
+            parent.appendChild(format_info_box);
+        }
     }
     else{
-        element.style.border="none";
-        format_info_box.style.display="none";
+        if(isActive){ // if format_info_box exists
+            element.style.border = "";
+            parent.removeChild(document.getElementById(element.id+"_format")); // remove(isActive) is equivalent but for semantic purposes it's clearer to do so
+        }
     }
 }
