b503194ce9fc51542a83b91d460752daecacb96d
Ethan <ethanpinto02@gmail.com>
2022-05-12 17:44:51 +0200

    starting verification login. (Not Finished Yet)

--- LaFleurWebsite/login.php
+++ LaFleurWebsite/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,23 +12,30 @@
 </head>
 <body>
         <div class="loginHead"></div>
-        <div class="loginDiv"><form name="login_form" id="login_form" method="post">
-            <table>
-                <tr>
-                    <td colspan="2" style="padding:10px ; text-align: center;"><h2>Se connecter à Wip.com<h2></td>
-                </tr>
-                <tr>
-                    <td style="padding:5px"><br>Mail:</td>
-                    <td style="padding:5px"><br><input type="text" name="mail" id="mail"></td>
-                </tr>
-                <tr>
-                    <td style="padding:5px">Password:</td>
-                    <td style="padding:5px"><input type="text" name="password" id="password"></td>
-                </tr>
-                <tr>
-                    <td colspan="2" style="padding:5px; text-align: center;"><input type="submit" value="Valider" style="padding:2px"></td>
-                </tr>
-            </table>
+        <div class="loginDiv"><form name="login_form" id="login_form" method="post" action="verification.php">
+            <fieldset>
+                <table>
+                    <tr>
+                        <td colspan="2" style="padding:10px ; text-align: center;"><h2>Se connecter à Wip.com<h2></td>
+                    </tr>
+                    <tr>
+                        <td style="padding:5px"><br>Mail:</td>
+                        <td style="padding:5px"><br><input placeholder="monmail@monsite.org" type="email" name="mail" id="mail" required></td>
+                    </tr>
+                    <tr>
+                        <td style="padding:5px">Password:</td>
+                        <td style="padding:5px"><input type="password" name="password" id="password" required></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="padding:5px; text-align: center;"><input type="submit" value="Valider" style="padding:2px"></td>
+                    </tr>
+                </table>
+                <?php 
+                    if(empty($_SESSION["currentUsername"])){
+                        echo "<p style=\"color:red; margin-top:5px; font-size: medium; text-align: center;\">Nom d'utilisateur ou mot de passe incorrect</p>";
+                    }
+                ?>
+            </fieldset>
         </form></div>
         <div class="loginFooter">
             <nav>
---
+++ LaFleurWebsite/verification.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+    $_SESSION["currentUsername"]="";
+    $loginsArray = array(
+        // [mail, password, username]
+        array("ethanpinto@orange.fr", "1234", "Hanabi"),
+        array("ethan", "0201", "Also_Hanabi"),
+        array("robin", "1302", "It's Robin")
+    );
+
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $username=$_REQUEST["mail"];
+        $password=$_REQUEST["password"];
+        
+        foreach($loginsArray as $login){
+            if($username==$login[0] && $password==$login[1]){
+                $_SESSION["currentUsername"]=$login[2];
+                header("Location: ./index.php", true);
+                exit();
+            }
+        }
+        header("Location: ./login.php", true);
+        exit();
+    }
+?>
+</body>
+</html>
