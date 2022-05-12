ff2fb6067d54d6d8de5fd49ebc142df8a175b00d
Ethan <ethanpinto02@gmail.com>
2022-05-12 15:31:49 +0200

    Préparation du login.php

---
+++ LaFleurWebsite/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<script src="js/login.js"></script>
+</head>
+<body>
+<div class="loginDiv"><form>
+    <table>
+        <tr>
+            <td>Mail:</td>
+            <td><input type="text" name="mail" id="mail"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="text" name="password" id="password"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Valider"></td>
+        </tr>
+    </table>
+</form></div>
+</body>
+</html>
