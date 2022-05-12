feaf4c0bd04a5bf6e442fa0080f6296c3c5b5b5b
Ethan <ethanpinto02@gmail.com>
2022-05-12 16:55:04 +0200

    Ajustation du login.php

--- LaFleurWebsite/login.php
+++ LaFleurWebsite/login.php
@@ -7,20 +7,31 @@
 	<script src="js/login.js"></script>
 </head>
 <body>
-<div class="loginDiv"><form>
-    <table>
-        <tr>
-            <td>Mail:</td>
-            <td><input type="text" name="mail" id="mail"></td>
-        </tr>
-        <tr>
-            <td>Password:</td>
-            <td><input type="text" name="password" id="password"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Valider"></td>
-        </tr>
-    </table>
-</form></div>
+        <div class="loginHead"></div>
+        <div class="loginDiv"><form>
+            <table>
+                <tr>
+                    <td colspan="2" style="padding:10px ; text-align: center;"><h2>Se connecter à Wip.com<h2></td>
+                </tr>
+                <tr>
+                    <td style="padding:5px"><br>Mail:</td>
+                    <td style="padding:5px"><br><input type="text" name="mail" id="mail"></td>
+                </tr>
+                <tr>
+                    <td style="padding:5px">Password:</td>
+                    <td style="padding:5px"><input type="text" name="password" id="password"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="padding:5px; text-align: center;"><input type="submit" value="Valider" style="padding:2px"></td>
+                </tr>
+            </table>
+        </form></div>
+        <div class="loginFooter">
+            <nav>
+                <ul class="loginFootMenu">
+                    <li><a href="index.php">Retourner à l'accueil</a></li>
+                </ul>
+            </nav>
+        </div>
 </body>
 </html>
