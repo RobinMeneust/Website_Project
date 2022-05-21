004b141f8c8127d79857059b1c84d734f04ec564
Ethan <ethanpinto02@gmail.com>
2022-05-21 15:56:40 +0200

    id generator and sort is working

--- php/createaccountWritting.php
+++ php/createaccountWritting.php
@@ -37,10 +37,26 @@
         }
    }
 
+    // generate the id for the new account
+    $prev = 0;
+    $nbUser = count($user);
+    $i = 0;
+    foreach($json as $user){
+        $idRead = (int) filter_var($user["id"], FILTER_SANITIZE_NUMBER_INT);
+        if($idRead != 0){  //don't check the case id=0 because it's the admin.
+            if($idRead != ($prev+1)){  // If we have found an unused id betwen 2 used, we can use it for the new utilisator
+                $id = "u".($prev+1);
+                break;
+            }
+            if(++$i === $nbUser){
+                $id = "u".($idRead+1);  // Case all id are already use, so we add an id +1 bigger than the biggest one. 
+                break;
+            }
+            $prev = $idRead;
+        }
+    }
+
     //Write the content of the form in the users.json
-
-    $id = "wip";
-
     $newUserData = array(
         "login" => $username,
 		"password" => $password,
@@ -55,6 +71,14 @@
 		"id" => $id
     );
     array_push($json,$newUserData);
+    //We have to short the new array
+    function comparator($object1, $object2){
+        $id1 = (int) filter_var($object1["id"], FILTER_SANITIZE_NUMBER_INT);
+        $id2 = (int) filter_var($object2["id"], FILTER_SANITIZE_NUMBER_INT);
+        return $id1 > $id2;
+    }
+    usort($json, 'comparator');
+
     $json = json_encode($json, JSON_PRETTY_PRINT);
     file_put_contents('../data/users.json',$json);
     header("Location: createaccount.php", true);
