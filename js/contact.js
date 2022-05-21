96c2b6e0c60876499e667420fcf64ecd2d59a69e
RobinMeneust <robin.meneust@hotmail.fr>
2022-05-21 16:49:09 +0200

    Improved contact script + theme update

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,19 @@
 function checkStringFormat(element){
-    let isNotValid = false;
     let parent = element.parentNode;
-    let isActive = document.getElementById(element.id+"_format");
-    //const regexWithAccents = new RegExp('[A-zÀ-ú0-9\-_@]*');
-    ///^([a-z0-9]{5,})$/
-    //const regexWithoutAccents = new RegExp('[A-z0-9\-_@]*');
+    let isActive = (document.getElementById(element.id+"_format")!=null);
+    const regex = new RegExp('^([A-Za-zÀ-ú0-9À-ÖØ-Ýà-öø-ý\' -]*)$');
+    //const regex_accents = new RegExp('[À-úÀ-ÖØ-Ýà-öø-ý]');
+    //const regex_numbers = new RegExp('[0-9]');
+    //const regex_special = new RegExp('[\' -]');
 
-    isNotValid = ! (/^([A-zÀ-ú0-9\-@]*)$/.test(element.value));
-    isNotValid |= / /.test(element.value);
-    if(isNotValid){
+
+    if(!regex.test(element.value)){ // if the format is incorrect
         if(!isActive){ // if format_info_box doesn't exists
             element.style.border = "solid red 1px";
             let format_info_box = document.createElement("span");
-            format_info_box.innerHTML = "Ne doit pas contenir de caractères spéciaux"
+            format_info_box.innerHTML = "Ne doit pas contenir de caractères spéciaux";
             format_info_box.style.color = "red";
-            format_info_box.id=element.id+"_format"
+            format_info_box.id=element.id+"_format";
 
             parent.appendChild(format_info_box);
         }
@@ -22,7 +21,7 @@ function checkStringFormat(element){
     else{
         if(isActive){ // if format_info_box exists
             element.style.border = "";
-            parent.removeChild(document.getElementById(element.id+"_format")); // remove(isActive) is equivalent but for semantic purposes it's clearer to do so
+            parent.removeChild(document.getElementById(element.id+"_format"));
         }
     }
 }
--- js/global.js
+++ js/global.js
@@ -3,13 +3,13 @@ function changeMenuDisplay(newState){
     let verticalMenuTextElements = document.getElementsByClassName("verticalMenuText");
 
     if(newState=="show"){
-        verticalMenu.style.width="17vw";
+        verticalMenu.style.width="330px";
         for(let i=0; i<verticalMenuTextElements.length; i++)
             verticalMenuTextElements[i].style.display="inline-block";
         document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('hide')");
     }
     else if(newState=="hide"){
-        verticalMenu.style.width="2vw";
+        verticalMenu.style.width="70px";
         for(let i=0; i<verticalMenuTextElements.length; i++)
             verticalMenuTextElements[i].style.display="none";
         document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('show')");
