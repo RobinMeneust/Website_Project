09fe2de97b44283c2375541c7b53d2f8268c2b00
Ethan <ethanpinto02@gmail.com>
2022-05-22 20:31:23 +0200

    correction of tabulation

--- php/prefab/footer.php
+++ php/prefab/footer.php
@@ -1,58 +1,58 @@
 <table class="footerContent" style="padding-top:30px">
-    <tr>
-        <th>Catégories</th>
-        <th>Gestion du compte</th>
-        <th>Autres pages</th>
-    </tr>
-    <tr>
-        <td>
-            <ul>
-                <?php
-                    $dataFile = fopen("data/categories.csv", "r") or die("ERROR the file data/categories.csv could not be opened");
-                    fgetcsv($dataFile, 100, ","); // Used to ignore the 1st line of the csv file
-                    while(($data = fgetcsv($dataFile, 100, ",")) !=FALSE){
-                        if(isset($data[1])) // We get the 2nd element (the name of the categorie)
-                            echo "<li><a href=\"categorie.php?catID=$data[0]&catName=$data[1]\">$data[1]</a></li>";
-                    }
-                    fclose($dataFile);
-                ?>
-            </ul>
-        </td>
-        <td>
-            <ul>
-                <li><a href=createAccount.php>Inscription</a></li>
-                <li><a href=login.php>Connexion</a></li>
-                <li><a href=profile.php>Profile</a></li>
-                <li><a href=php/logout.php>Déconnexion</a></li>
-            </ul>
-        </td>
-        <td>
-            <ul>
-                <li><a href=index.php>Accueil</a></li>
-                <li><a href=about.php>À propos</a></li>
-                <li><a href=contact.php>Nous contacter</a></li>
-            </ul>
-        </td>
-    </tr>
+	<tr>
+		<th>Catégories</th>
+		<th>Gestion du compte</th>
+		<th>Autres pages</th>
+	</tr>
+	<tr>
+		<td>
+			<ul>
+				<?php
+					$dataFile = fopen("data/categories.csv", "r") or die("ERROR the file data/categories.csv could not be opened");
+					fgetcsv($dataFile, 100, ","); // Used to ignore the 1st line of the csv file
+					while(($data = fgetcsv($dataFile, 100, ",")) !=FALSE){
+						if(isset($data[1])) // We get the 2nd element (the name of the categorie)
+							echo "<li><a href=\"categorie.php?catID=$data[0]&catName=$data[1]\">$data[1]</a></li>";
+					}
+					fclose($dataFile);
+				?>
+			</ul>
+		</td>
+		<td>
+			<ul>
+				<li><a href=createAccount.php>Inscription</a></li>
+				<li><a href=login.php>Connexion</a></li>
+				<li><a href=profile.php>Profile</a></li>
+				<li><a href=php/logout.php>Déconnexion</a></li>
+			</ul>
+		</td>
+		<td>
+			<ul>
+				<li><a href=index.php>Accueil</a></li>
+				<li><a href=about.php>À propos</a></li>
+				<li><a href=contact.php>Nous contacter</a></li>
+			</ul>
+		</td>
+	</tr>
 </table>
 <table class="footerContent" style="border:none">
-    <tr>
-        <th>Concepteurs</th>
-        <th>Mention légale</th>
-        <th>Contact</th>
-    </tr>
-    <tr>
-        <td>
-            <ul>
-                <li>Romain BARRÉ</li>
-                <li>Alexis DUVERGER</li>
-                <li>Robin MENEUST</li>
-                <li>Ethan PINTO</li>
-            </ul>
-        </td>
-        <td><ul><li>© Mini U</li></ul></td>
-        <td>
-            <address><ul><li>websiteprojet2022@gmail.com</li></ul></address>
-        </td>
-    </tr>
+	<tr>
+		<th>Concepteurs</th>
+		<th>Mention légale</th>
+		<th>Contact</th>
+	</tr>
+	<tr>
+		<td>
+			<ul>
+				<li>Romain BARRÉ</li>
+				<li>Alexis DUVERGER</li>
+				<li>Robin MENEUST</li>
+				<li>Ethan PINTO</li>
+			</ul>
+		</td>
+		<td><ul><li>© Mini U</li></ul></td>
+		<td>
+			<address><ul><li>websiteprojet2022@gmail.com</li></ul></address>
+		</td>
+	</tr>
 </table>
--- profile.php
+++ profile.php
@@ -19,37 +19,35 @@
 		<table style="padding:5px; margin-bottom:10px; width:100%;">
 			<tr>
 				<td>Pseudonyme : </td>
-				<td><?php echo ($_SESSION["currentUser"]["login"]); ?></td>
-				<td style="padding-left:10px;">Modifier</td>
+				<td><?php echo ($_SESSION["currentUser"]["login"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Prénom : </td>
-				<td><?php echo ($_SESSION["currentUser"]["first_name"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["first_name"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Nom : </td>
-				<td><?php echo ($_SESSION["currentUser"]["last_name"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["last_name"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Genre : </td>
-				<td><?php echo ($_SESSION["currentUser"]["gender"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["gender"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Mail : </td>
-				<td><?php echo ($_SESSION["currentUser"]["email"]); ?></td>
-				<td style="padding-left:10px;">Modifier</td>
+				<td><?php echo ($_SESSION["currentUser"]["email"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Téléphone : </td>
-				<td><?php echo ($_SESSION["currentUser"]["phone_nb"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["phone_nb"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Date de naissance : </td>
-				<td><?php echo ($_SESSION["currentUser"]["birth_date"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["birth_date"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Adresse : </td>
-				<td><?php echo ($_SESSION["currentUser"]["address"]); ?></td>
+				<td><?php echo ($_SESSION["currentUser"]["address"]); ?></td>				
 			</tr>
 			<tr>
 				<td>Fonction : </td>
@@ -61,13 +59,13 @@
 			</tr>
 			<tr>
 				<td>Mot de Passe : </td>
-				<td><?php echo ("********");?></td>
-				<td style="padding-left:0px;">Afficher</td>
-				<td style="padding-left:0px;">Modifier</td>
+				<td><div class="profilPassword"><?php echo ("********");?></div></td>
+				<td><button class="showPassword" onclick="">Afficher</button></td>
 			</tr>
 		</table>
 		<a href="php/logout.php" tite="Logout" style="margin-left:10px; padding:5px;">Se déconnecter</a>
 		<a href="index.php" tite="Index" style="margin-left:10px; padding:5px;">Accueil</a>
+		<a href="editProfil.php" tite="EditProfil" style="margin-left:10px; padding:5px;">Modifier le profil</a>
 	</div>
 </body>
 </html>
