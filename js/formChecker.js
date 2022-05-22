61e3d16a65e09f52a4a8c033ece8ade092699aa9
Ethan <ethanpinto02@gmail.com>
2022-05-22 21:05:19 +0200

    correction of bad tabulation

--- js/formChecker.js
+++ js/formChecker.js
@@ -1,54 +1,54 @@
 function checkStringFormat(element, mode="default"){
-    let parent = element.parentNode;
-    let isActive = (document.getElementById(element.id+"_format")!=null);
-    let isIncorrect = false;
-    let errorMsg="";
-    const regex_default = new RegExp('^([A-Za-zÀ-ú0-9À-ÖØ-Ýà-öø-ý\' -]*)$');
-    const regex_tel = new RegExp('^([0-9]*)$');
-    const regex_username = new RegExp('^([A-Za-z0-9_]*)$');
-    const regex_passwd = new RegExp('^([A-Za-z0-9À-ÖØ-Ýà-öø-ý~!@#$%^&*_+=<>.?/-]*)$');
+	let parent = element.parentNode;
+	let isActive = (document.getElementById(element.id+"_format")!=null);
+	let isIncorrect = false;
+	let errorMsg="";
+	const regex_default = new RegExp('^([A-Za-zÀ-ú0-9À-ÖØ-Ýà-öø-ý\' -]*)$');
+	const regex_tel = new RegExp('^([0-9]*)$');
+	const regex_username = new RegExp('^([A-Za-z0-9_]*)$');
+	const regex_passwd = new RegExp('^([A-Za-z0-9À-ÖØ-Ýà-öø-ý~!@#$%^&*_+=<>.?/-]*)$');
 
-    if(element.value==""){
-        isIncorrect = true;
-        errorMsg = "<br>Ne doit pas être vide";
-    }
-    else if(mode=="tel"){
-        isIncorrect = !regex_tel.test(element.value);
-        errorMsg = "<br>Ne doit contenir que des chiffres";
-    }
-    else if(mode=="username"){
-        isIncorrect = !regex_username.test(element.value);
-        errorMsg = "<br>Ne doit contenir que des lettres, des chiffres ou le caractère : _";
-    }
-    else if(mode=="passwd"){
-        isIncorrect = !regex_passwd.test(element.value);
-        errorMsg = "<br>Ne doit contenir que des lettres (accents acceptés), des chiffres ou les caractères : ~!@#$%^&*_+=<>.?/-";
-    }
-    else{
-        isIncorrect = !regex_default.test(element.value);
-        errorMsg = "<br>Ne doit pas contenir de caractères spéciaux (mais plusieurs accents sont autorisés)";
-    }
+	if(element.value==""){
+		isIncorrect = true;
+		errorMsg = "<br>Ne doit pas être vide";
+	}
+	else if(mode=="tel"){
+		isIncorrect = !regex_tel.test(element.value);
+		errorMsg = "<br>Ne doit contenir que des chiffres";
+	}
+	else if(mode=="username"){
+		isIncorrect = !regex_username.test(element.value);
+		errorMsg = "<br>Ne doit contenir que des lettres, des chiffres ou le caractère : _";
+	}
+	else if(mode=="passwd"){
+		isIncorrect = !regex_passwd.test(element.value);
+		errorMsg = "<br>Ne doit contenir que des lettres (accents acceptés), des chiffres ou les caractères : ~!@#$%^&*_+=<>.?/-";
+	}
+	else{
+		isIncorrect = !regex_default.test(element.value);
+		errorMsg = "<br>Ne doit pas contenir de caractères spéciaux (mais plusieurs accents sont autorisés)";
+	}
 
-    console.log(isIncorrect);
+	console.log(isIncorrect);
 
-    if(isIncorrect){ // if the format is incorrect
-        if(!isActive){ // if format_info_box doesn't exists
-            element.style.border = "solid red 1px";
-            let format_info_box = document.createElement("span");
-            format_info_box.innerHTML = errorMsg;
-            format_info_box.style.color = "red";
-            format_info_box.id=element.id+"_format";
+	if(isIncorrect){ // if the format is incorrect
+		if(!isActive){ // if format_info_box doesn't exists
+			element.style.border = "solid red 1px";
+			let format_info_box = document.createElement("span");
+			format_info_box.innerHTML = errorMsg;
+			format_info_box.style.color = "red";
+			format_info_box.id=element.id+"_format";
 
-            parent.appendChild(format_info_box);
-        }
-        else{
-            document.getElementById(element.id+"_format").innerHTML=errorMsg;
-        }
-    }
-    else{
-        if(isActive){ // if format_info_box exists
-            element.style.border = "";
-            parent.removeChild(document.getElementById(element.id+"_format"));
-        }
-    }
+			parent.appendChild(format_info_box);
+		}
+		else{
+			document.getElementById(element.id+"_format").innerHTML=errorMsg;
+		}
+	}
+	else{
+		if(isActive){ // if format_info_box exists
+			element.style.border = "";
+			parent.removeChild(document.getElementById(element.id+"_format"));
+		}
+	}
 }
--- js/global.js
+++ js/global.js
@@ -1,17 +1,17 @@
 function changeMenuDisplay(newState){
-    let verticalMenu = document.getElementsByClassName("verticalMenu")[0];
-    let verticalMenuTextElements = document.getElementsByClassName("verticalMenuText");
+	let verticalMenu = document.getElementsByClassName("verticalMenu")[0];
+	let verticalMenuTextElements = document.getElementsByClassName("verticalMenuText");
 
-    if(newState=="show"){
-        verticalMenu.style.width="330px";
-        for(let i=0; i<verticalMenuTextElements.length; i++)
-            verticalMenuTextElements[i].style.display="inline-block";
-        document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('hide')");
-    }
-    else if(newState=="hide"){
-        verticalMenu.style.width="70px";
-        for(let i=0; i<verticalMenuTextElements.length; i++)
-            verticalMenuTextElements[i].style.display="none";
-        document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('show')");
-    }
+	if(newState=="show"){
+		verticalMenu.style.width="330px";
+		for(let i=0; i<verticalMenuTextElements.length; i++)
+			verticalMenuTextElements[i].style.display="inline-block";
+		document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('hide')");
+	}
+	else if(newState=="hide"){
+		verticalMenu.style.width="70px";
+		for(let i=0; i<verticalMenuTextElements.length; i++)
+			verticalMenuTextElements[i].style.display="none";
+		document.getElementsByClassName("verticalMenuButton")[0].setAttribute("onclick", "changeMenuDisplay('show')");
+	}
 }
